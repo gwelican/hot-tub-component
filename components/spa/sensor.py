8e64f54a47910a0067b447b22a3f8f5489fab4c1
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2025-03-24 14:51:29 -0700

    fix

--- components/spa/sensor.py
+++ components/spa/sensor.py
@@ -3,49 +3,47 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     CONF_ID,
+    CONF_NAME,
+    CONF_TYPE,
     DEVICE_CLASS_TEMPERATURE,
     STATE_CLASS_MEASUREMENT,
     UNIT_CELSIUS,
 )
 from . import spa_ns, SpaReader, CONF_SPA_ID
 
-SENSORS = {
-    "temp_sensor": ("Current Temp", UNIT_CELSIUS, 1),
-    "target_temp_sensor": ("Target Temp", UNIT_CELSIUS, 1),
-    "jet1_sensor": ("Jet1", None, 0),
-    "jet2_sensor": ("Jet2", None, 0),
-    "jet3_sensor": ("Jet3", None, 0),
-    "light_sensor": ("Light", None, 0),
-    "restmode_sensor": ("Rest Mode", None, 0),
-    "highrange_sensor": ("High Range", None, 0),
-    "hour_sensor": ("Hour", None, 0),
-    "minute_sensor": ("Minute", None, 0),
-    "heater_sensor": ("Heater", None, 0),
-    "circ_sensor": ("Circulation Pump", None, 0),
-    "filt1hour_sensor": ("Filter 1 Hour", None, 0),
-    "filt1minute_sensor": ("Filter 1 Minute", None, 0),
-    "filt1durhour_sensor": ("Filter 1 Duration Hour", None, 0),
-    "filt1durminute_sensor": ("Filter 1 Duration Minute", None, 0),
-    "filt2enable_sensor": ("Filter 2 Enable", None, 0),
-    "filt2hour_sensor": ("Filter 2 Hour", None, 0),
-    "filt2minute_sensor": ("Filter 2 Minute", None, 0),
-    "filt2durhour_sensor": ("Filter 2 Duration Hour", None, 0),
-    "filt2durminute_sensor": ("Filter 2 Duration Minute", None, 0),
+SENSOR_TYPES = {
+    "temp_sensor": (UNIT_CELSIUS, 1, DEVICE_CLASS_TEMPERATURE),
+    "target_temp_sensor": (UNIT_CELSIUS, 1, DEVICE_CLASS_TEMPERATURE),
+    "jet1_sensor": (None, 0, None),
+    "jet2_sensor": (None, 0, None),
+    "jet3_sensor": (None, 0, None),
+    "light_sensor": (None, 0, None),
+    "restmode_sensor": (None, 0, None),
+    "highrange_sensor": (None, 0, None),
+    "hour_sensor": (None, 0, None),
+    "minute_sensor": (None, 0, None),
+    "heater_sensor": (None, 0, None),
+    "circ_sensor": (None, 0, None),
+    "filt1hour_sensor": (None, 0, None),
+    "filt1minute_sensor": (None, 0, None),
+    "filt1durhour_sensor": (None, 0, None),
+    "filt1durminute_sensor": (None, 0, None),
+    "filt2enable_sensor": (None, 0, None),
+    "filt2hour_sensor": (None, 0, None),
+    "filt2minute_sensor": (None, 0, None),
+    "filt2durhour_sensor": (None, 0, None),
+    "filt2durminute_sensor": (None, 0, None),
 }
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_SPA_ID): cv.use_id(SpaReader),
-    cv.Optional(sensor_id): sensor.sensor_schema(
-        unit_of_measurement=unit,
-        accuracy_decimals=accuracy,
-        device_class=DEVICE_CLASS_TEMPERATURE if "temp" in sensor_id else None,
-    ) for sensor_id, (name, unit, accuracy) in SENSORS.items()
-})
+CONFIG_SCHEMA = sensor.sensor_schema().extend(
+    {
+        cv.GenerateID(CONF_SPA_ID): cv.use_id(SpaReader),
+        cv.Required(CONF_TYPE): cv.enum(SENSOR_TYPES, upper=False),
+    }
+)
+
 
 async def to_code(config):
     spa = await cg.get_variable(config[CONF_SPA_ID])
-    
-    for sensor_id, (name, unit, accuracy) in SENSORS.items():
-        if sensor_id in config:
-            sens = await sensor.new_sensor(config[sensor_id])
-            cg.add(spa.set_sensor(sensor_id, sens))
+    var = await sensor.new_sensor(config)
+    cg.add(spa.set_sensor(config[CONF_TYPE], var))
